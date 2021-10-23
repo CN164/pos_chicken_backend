43be95807b4d763b7c8d7bb296ddffa86d557210
Purin Tadsorn <purintadsorn@gmail.com>
2021-10-23 13:34:49 +0700

    edit-data

--- Veda/BussinessFlow/OrderFlow.cs
+++ Veda/BussinessFlow/OrderFlow.cs
@@ -13,6 +13,7 @@ namespace pos_chicken_backend.BussinessFlow
         private readonly IBaseRepository baseRepository;
         public int queueID;
         public List<OrderEntity> OrderDataQ;
+        public List<StockEntity> StockData;
         public OrderBussinessFlow(IBaseRepository baseRepository)
         {
             this.baseRepository = baseRepository;
@@ -24,7 +25,12 @@ namespace pos_chicken_backend.BussinessFlow
         public List<OrderResponse> Orderbuy(List<OrderRequest> request)
         {
             List<OrderEntity> OrderData = this.baseRepository.Gets<OrderEntity>();
-            List<StockEntity> StockData = this.baseRepository.Gets<StockEntity>();
+            foreach (OrderRequest item in request)
+            {
+                //ดึงข้อมูลมา แล้วไป logic ไปแมพแค่ id เท่านั้นแล้ว ค่อยไป calstock
+                //หรือ ก็แอด list นี้ลงให้ได้ในขั้นตอนนี่เพื่อจบทุกอย่างซ่ะ
+                List<StockEntity> StockData = this.baseRepository.Gets<StockEntity>(x => x.id == item.stockId);
+            }
             if (OrderData.Count > 0)
             {
                 this.queueID = this.baseRepository.Gets<OrderEntity>().LastOrDefault().queueOrder;
@@ -56,6 +62,16 @@ namespace pos_chicken_backend.BussinessFlow
             
 
             return null;
+        }
+        public List<OrderReportResponse> orderReportResponse()
+        {
+            List<OrderEntity> OrderDatas = this.baseRepository.Gets<OrderEntity>();
+            List<StockEntity> StockDatas = this.baseRepository.Gets<StockEntity>();
+
+            OrderLogic orderLogic = new OrderLogic();
+            List<OrderReportResponse> OrderReportResponse = orderLogic.orderReportResponses(OrderDatas, StockDatas);
+
+            return OrderReportResponse;
         }
     }
 }
---
+++ Veda/Models/Response/OrderReportResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pos_chicken_backend.Models
+{
+    public class OrderReportResponse
+    {
+        public int id { get; set; }
+        public string stokcName { get; set; }
+        public int totalUnit { get; set; }
+        public int totalPrice { get; set; }
+    }
+}
