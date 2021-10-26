b9a2ac430351e428d07f1ec8f878708e44d27f93
narawitP <62659583+narawitP@users.noreply.github.com>
2021-10-26 21:07:43 +0700

    add entity

--- Veda/Models/Response/QueueOrderResponse.cs
+++ Veda/Models/Response/QueueOrderResponse.cs
@@ -8,7 +8,7 @@ namespace pos_chicken_backend.Models
     public class QueueOrderResponse
     {
         public int queue { get; set; }
-        public List<StockEntity> products { get; set; }
+        public List<OrderProductDetailResponse> products { get; set; }
         public StateEntity state { get; set; }
     }
 }
